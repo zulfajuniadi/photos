b3044e409733f6cb489d86e66fe179025b57e33d
Zulfa Juniadi Zulkifli <zulfajuniadi@gmail.com>
2014-04-09 22:32:25 +0800

    update public path

--- app/views/albums/index.blade.php
+++ app/views/albums/index.blade.php
@@ -1,6 +1,29 @@
 @extends('layout')
 
 @section('content')
+<style>
+.albums > div {
+  height: 180px;
+  position: relative;
+  width: 180px;
+  display: inline-block;
+  vertical-align: top;
+  overflow: hidden;
+  text-align: center;
+}
+.albums > div img {
+  height: 180px;
+}
+.albums > div footer {
+  position: absolute;
+  bottom: 0px;
+  left: 0px;
+  width: 100%;
+  text-align: center;
+  background-color: rgba(255, 255, 255, 0.8);
+}
+</style>
+
 <div class="container-fluid albums">
   @foreach($albums as $album)
     <div><a href="{{action('AlbumsController@getView', $album->id)}}">
